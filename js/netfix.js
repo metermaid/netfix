0ed30196e9522b8a13b8d167e374b54b900b010a
Rita Cheng <rita@ritacheng.com>
2013-12-06 18:26:50 -0800

    changed sliders to a hide/show toggle button, fixed alignment issues on homepage, restrict script to a few certain pages, refactored a little

--- js/netfix.js
+++ js/netfix.js
@@ -1,40 +1,82 @@
-function getRating(url,boxArt) {
+var isHome = $('body').is('#page-WiHome');
+var isGenre = $('body').is('#page-WiGenre') || $('body').is('#page-WiAltGenre');
+var isRoleDisplay = $('body').is('#page-WiRoleDisplay');
+
+function getRating(url,boxArt,callback) {
 	$.get(url, function(data){
 		var html = jQuery('<div>').html(data);
 		var starbar = html.find('.starbar:first').find('span:first');
 		boxArt.append(starbar);
-		var starClass = starbar.find('span').attr('class');//.match(/sbmf-[0-9]{2}/)[1];
-
-		// hide poorly rated
-		if (/sbmf-[01][0-9]|sbmf-2[0-5]/.test(starClass)) {
-			boxArt.parent().hide();
-		}
-		// hide self rated
-		if (/sbmfrt/.test(starClass)) {
-			boxArt.parent().hide();
-		}
+		var classes = starbar.find('span').attr('class');
+		if (!isRoleDisplay)
+			hideBadMovie(classes,boxArt);
+		if (isHome)
+			hideSelfRated(classes,boxArt);
 	});
 }
 
-$(document).ready(function(){
-	// Remove all sliders in favour of plain ol' boxes
-	
+function hideBadMovie(classes,boxArt) {
+		if (/sbmf-[01][0-9]|sbmf-2[0-5]/.test(classes)) {
+			boxArt.parent().hide();
+	}
+}
+
+function hideSelfRated(classes,boxArt) {
+		if (/sbmfrt/.test(classes)) {
+			boxArt.parent().hide();
+	}
+}
+
+function cleanUpHome() {
 	//not sure if I like this
 	$('.slider').each(function(){
 		$(this).removeClass('triangleBtns');
 		$('.boxShotDivider').remove();
 		$(this).removeClass('slider');
 	});
-	
+
 	// remove the facebook row
 	$('.mrow.fb').remove();
 
-	$('a[href*=WiPlayer]').each(function(){
-		var url = $(this).attr('href');
-		url = url.replace(/WiPlayer\?movieid=/i, "WiMovie/");
-		url = url.replace(/\&/i, "?");
-		$(this).attr('href',url);
-		$(this).css('background-image', 'none');
-		getRating(url,$(this).parent());
-	})
+	// hide annotations for even rows
+	$('.videoAnnotation').hide();
+}
+
+function cleanUpURL(url) {
+	url = url.replace(/WiPlayer\?movieid=/i, "WiMovie/");
+	url = url.replace(/\&/i, "?");
+	return url;
+}
+
+$(document).ready(function(){
+	// Remove all sliders in favour of plain ol' boxes
+	
+	if (isHome) {
+		cleanUpHome();
+	}
+
+	if (isHome || isGenre || isRoleDisplay) {
+		$('a[href*=WiPlayer]').each(function(){
+			var url = cleanUpURL($(this).attr('href'));
+			$(this).attr('href',url);
+			$(this).css('background-image', 'none');
+			getRating(url,$(this).parent());
+		});
+		if (isHome) {
+			$('.agMovieSet').each (function () {
+	    	$(this).addClass('truncated');
+	    	$(this).after('<a href="#" class="toggleMovies">Show More</a>');
+	    	$('.toggleMovies').click( function(e){
+	    		e.preventDefault();
+	    		$(this).prev('.agMovieSet').toggleClass('truncated');
+	    		if ($(this).text() == "Show More")
+	    			$(this).text("Hide More");
+	    		else
+	    			$(this).text("Show More");
+	    	});
+			});
+		}
+		
+	}
+
 });
